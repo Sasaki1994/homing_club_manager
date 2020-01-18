9e657830684f5f0f9b245e26b8f7240a6bfe40f1
Sasaki1994 <b.92421933411@gmail.com>
2020-01-18 16:23:26 +0900

    設定変更バグ修正

--- app/controllers/line_bot_controller.rb
+++ app/controllers/line_bot_controller.rb
@@ -3,7 +3,7 @@ class LineBotController < ApplicationController
     require 'json'
     protect_from_forgery :except => [:callback, :test]
 
-    
+    @@client ||= Line::Bot::Client.new { |config|
       config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
       config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
     }
